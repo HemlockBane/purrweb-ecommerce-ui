e765511c32339ee1ef7f8901f5c31f6d8afbd1e0
Obinna Igwe <igwenus619@gmail.com>
2021-02-19 12:22:38 +0100

    Add images for top layer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,41 @@
-import 'file:///C:/Users/igwen/Code%20Projects/IdeaProjects/constraints_tutorial/lib/widgets/theme.dart';
+import 'package:constraints_tutorial/widgets/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+class Item {
+  Item({this.name, this.price, this.discount, this.imagePath});
+
+  String name = "";
+  int price = 0;
+  int discount = 0;
+  String imagePath = "";
+}
+
+class AppImages {
+  static String _basePath = "assets/images";
+  static String dami = "$_basePath/dami.jpg";
+  static String eliDafaria = "$_basePath/eli_defaria.jpg";
+  static String marionelLuciano = "$_basePath/marionel_luciano.jpg";
+}
+
+List<Item> getPlaceHolderItems() {
+  return <Item>[
+    Item(
+        name: "Winter Coat", price: 80, discount: 0, imagePath: AppImages.dami),
+    Item(
+        name: "Woolen Coat",
+        price: 80,
+        discount: 30,
+        imagePath: AppImages.eliDafaria),
+    Item(
+        name: "Winter Coat", price: 80, discount: 0, imagePath: AppImages.dami),
+    Item(
+        name: "Woolen Coat",
+        price: 80,
+        discount: 30,
+        imagePath: AppImages.eliDafaria)
+  ];
+}
 
 void main() {
   runApp(MyApp());
@@ -27,65 +62,199 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final tabItems = ["Coats", "Dresses", "Jersey", "Pants"];
+  var selectedTabIdx = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SizedBox.expand(
-          child: DefaultTabController(
-            length: 4,
-            child: Container(
-              padding: EdgeInsets.only(left: 15, right: 15, top: 15),
-              child: Column(
-                children: [
-                  buildAppBar(),
-                  Flexible(
-                    child: FractionallySizedBox(
-                      heightFactor: 0.08,
+          child: Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 15, right: 15, top: 15),
+                child: Column(
+                  children: [
+                    buildAppBar(),
+                    SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  Row(
-                    children: [
-                      InkWell(
-                        onTap: () {},
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 11, horizontal: 14),
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(18),
-                            ),
-                          ),
-                          child: Text('Coats'),
-                        ),
-                      )
-                    ],
-                  ),
-                  TabBar(
-                    tabs: [
-                      Tab(text: 'Coats'),
-                      Tab(text: 'Dresses'),
-                      Tab(text: 'Jerseys'),
-                      Tab(text: 'Pants')
-                    ],
-                  ),
-                  Expanded(
-                    child: TabBarView(
-                      children: [
-                        Tab(text: 'Popular'),
-                        Tab(text: 'A-Z'),
-                        Tab(text: 'Last viewed'),
-                        Tab(text: 'Last viewed')
-                      ],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: List.generate(
+                          4, (int idx) => _buildTab(tabItems[idx], idx)),
                     ),
-                  )
-                ],
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _buildCatalog()
+                  ],
+                ),
               ),
-            ),
+              Align(
+                alignment: Alignment(0.0, 0.9),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(28),
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.search,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ), //
+    );
+  }
+
+  Expanded _buildCatalog() {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 300,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: List.generate(
+                  4,
+                  (int idx) {
+                    final items = getPlaceHolderItems();
+                    final item = items[idx];
+                    return Container(
+                      margin: EdgeInsets.only(right: 17),
+                      height: 200,
+                      width: 190,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(item.imagePath),
+                                        fit: BoxFit.cover),
+                                    color: Colors.amber,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(18),
+                                    ),
+                                  ),
+                                ),
+                                if (item.discount != 0)
+                                  Positioned(
+                                    top: 7,
+                                    left: 10,
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: 5, horizontal: 10),
+                                      child: Text(
+                                        '-${item.discount}%',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: Colors.black,
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(8),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('${item.name}'),
+                                Text("\$${item.price}")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 17,
+            ),
+            Text("Reccommended for You"),
+            SizedBox(
+              height: 17,
+            ),
+            Container(
+              height: 230,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: List.generate(
+                  4,
+                  (int idx) => Container(
+                    margin: EdgeInsets.only(right: 17),
+                    height: 100,
+                    width: 170,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  InkWell _buildTab(String text, int idx) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          selectedTabIdx = idx;
+        });
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 11, horizontal: 14),
+        decoration: selectedTabIdx != idx
+            ? null
+            : BoxDecoration(
+                color: AppTheme.pink,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(18),
+                ),
+              ),
+        child: Text(text),
+      ),
     );
   }
 
@@ -101,10 +270,16 @@ class _MyHomePageState extends State<MyHomePage> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
-          height: 55,
-          width: 55,
-          decoration:
-              BoxDecoration(shape: BoxShape.circle, color: Colors.black),
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.black,
+            image: DecorationImage(
+              image: AssetImage(AppImages.marionelLuciano,),
+              fit: BoxFit.cover
+            ),
+          ),
         ),
         Stack(
           fit: StackFit.loose,
@@ -115,7 +290,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: IconButton(
                 icon: Icon(
                   Icons.shopping_cart_rounded,
-                  size: 35,
+                  size: 30,
                   color: Colors.black,
                 ),
                 onPressed: null,
@@ -130,7 +305,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   vertical: 7,
                 ),
                 decoration: BoxDecoration(
-                  color: Colors.red,
+                  color: AppTheme.pink,
                   shape: BoxShape.circle,
                 ),
                 child: Text('2'),
--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -2,21 +2,13 @@ import 'package:constraints_tutorial/widgets/bubble_tab_indicator.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static const Color black = Color(0xff333333);
+  static const Color grey = Color(0xff828282);
+  static const Color pink = Color(0xffF8D9E0);
+
   ThemeData getDefaultAppTheme() {
     final ThemeData baseTheme = ThemeData.light();
 
-    return baseTheme.copyWith(
-      tabBarTheme: TabBarTheme(
-          labelColor: Colors.black,
-          unselectedLabelColor: Colors.grey,
-          labelStyle: baseTheme.textTheme.bodyText1.copyWith(),
-          indicator: BubbleTabIndicator(
-            // indicatorColor: Colors.green,
-            indicatorHeight: 13,
-            indicatorRadius: 15,
-            tabBarIndicatorSize: TabBarIndicatorSize.label,
-          ),
-          indicatorSize: TabBarIndicatorSize.label),
-    );
+    return baseTheme.copyWith();
   }
 }
